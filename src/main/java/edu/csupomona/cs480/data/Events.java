0cbd843e3257c06dcda26284f3f2e92ae4c91fac
Unknown <pakasteiner@gmail.com>
2017-11-01 21:09:39 -0700

    - ''

--- src/main/java/edu/csupomona/cs480/data/Events.java
+++ src/main/java/edu/csupomona/cs480/data/Events.java
@@ -1,5 +1,48 @@
 package edu.csupomona.cs480.data;
 
 public class Events {
-
+	private String EventId;
+	private String EventName;
+	private Organizations Org;
+	private String Planner;
+	private String Date;
+	private boolean isPublic;
+	
+	public String getEventId() {
+		return EventId;
+	}
+	public void setEventId(String eventId) {
+		EventId = eventId;
+	}
+	public String getEventName() {
+		return EventName;
+	}
+	public void setEventName(String eventName) {
+		EventName = eventName;
+	}
+	public Organizations getOrg() {
+		return Org;
+	}
+	public void setOrg(Organizations org) {
+		Org = org;
+	}
+	public String getPlanner() {
+		return Planner;
+	}
+	public void setPlanner(String planner) {
+		Planner = planner;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public boolean isPublic() {
+		return isPublic;
+	}
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	
 }
--- src/main/java/edu/csupomona/cs480/data/Organizations.java
+++ src/main/java/edu/csupomona/cs480/data/Organizations.java
@@ -1,5 +1,19 @@
 package edu.csupomona.cs480.data;
 
 public class Organizations {
-
+	private String OrgId;
+	private String OrgName;
+	
+	public String getOrgId() {
+		return OrgId;
+	}
+	public void setOrgId(String orgId) {
+		OrgId = orgId;
+	}
+	public String getOrgName() {
+		return OrgName;
+	}
+	public void setOrgName(String orgName) {
+		OrgName = orgName;
+	}
 }
