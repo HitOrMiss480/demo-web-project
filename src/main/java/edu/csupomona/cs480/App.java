3072361cd120eba0db3012a5c60b179eca43d26d
David <djshin94@gmail.com>
2017-11-09 03:01:51 -0800

    testing google auth

--- src/main/java/edu/csupomona/cs480/App.java
+++ src/main/java/edu/csupomona/cs480/App.java
@@ -1,13 +1,23 @@
 package edu.csupomona.cs480;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.api.services.appsactivity.Appsactivity;
+import com.google.api.services.appsactivity.model.Activity;
+import com.google.api.services.appsactivity.model.Event;
+import com.google.api.services.appsactivity.model.ListActivitiesResponse;
+import com.google.api.services.appsactivity.model.Target;
+import com.google.api.services.appsactivity.model.User;
+
 import edu.csupomona.cs480.data.provider.FSUserManager;
 import edu.csupomona.cs480.data.provider.UserManager;
+
 
 @Configuration
 @EnableAutoConfiguration
@@ -33,8 +43,13 @@ public class App {
      * other main methods for testing or debugging purposes, but
      * you cannot put extra main method when building your project.
      */
+    
     public static void main(String[] args) throws Exception {
         // Run Spring Boot
         SpringApplication.run(App.class, args);
-    }
-}
+        
+            }            
+}	
+    
+
+    
